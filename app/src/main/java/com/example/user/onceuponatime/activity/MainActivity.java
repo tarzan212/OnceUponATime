9cdbe3c086b34f8a9711d30c93d46e6836552aad
tarzan212 <activation212@gmail.com>
2017-03-13 20:03:47 +0100

    Added some code to Helpers

--- app/src/main/java/com/example/user/onceuponatime/activity/MainActivity.java
+++ app/src/main/java/com/example/user/onceuponatime/activity/MainActivity.java
@@ -11,6 +11,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -21,11 +22,15 @@ import android.widget.Toast;
 import com.example.user.onceuponatime.R;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseUser mUser;
+    private DatabaseReference mDatabaseReference;
+
+    private String mUid;
 
     private View navHeader;
 
@@ -35,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout drawerLayout;
     private TextView nameField,storiesCountField;
     private Toolbar mToolbar;
+    private RecyclerView mRecycler;
 
     private String[] activityTitles;
 
@@ -55,9 +61,13 @@ public class MainActivity extends AppCompatActivity {
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+        } else {
+            mUid = mUser.getUid();
         }
 
 
+
+        mRecycler =(RecyclerView) findViewById(R.id.main_recycler);
         navigationView = (NavigationView) findViewById(R.id.nav_main);
         floatingActionButton = (FloatingActionButton) findViewById(R.id.floatingbutton);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout_main);
--- app/src/main/java/com/example/user/onceuponatime/other/UserHelper.java
+++ app/src/main/java/com/example/user/onceuponatime/other/UserHelper.java
@@ -1,8 +1,81 @@
 package com.example.user.onceuponatime.other;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.widget.TextView;
+
+import java.util.Map;
+
 /**
  * Created by User on 11/03/2017.
  */
 
 public class UserHelper {
+
+    private String userID;
+    private String username;
+    private String email;
+    private long date;
+    private Map<String,String> stories;
+    private Map<String,Map<String,String>> invStories;
+
+    public UserHelper() {};
+
+    public UserHelper(@NonNull String userid, String usnme, @NonNull String email,
+                      long date,Map<String,String> stories,Map<String,Map<String,String>> involving) {
+        this.userID = userid;
+        this.username = usnme;
+        this.email = email;
+        this.date = date;
+        this.stories = stories;
+        this.invStories = involving;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public Map<String, String> getStories() {
+        return stories;
+    }
+
+    public void setStories(Map<String, String> stories) {
+        this.stories = stories;
+    }
+
+    public Map<String, Map<String, String>> getInvStories() {
+        return invStories;
+    }
+
+    public void setInvStories(Map<String, Map<String, String>> invStories) {
+        this.invStories = invStories;
+    }
 }
